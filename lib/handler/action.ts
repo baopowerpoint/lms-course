6e7ad0f64f608091d90180ab1a9b1767226321a5
baopowerpoint <baopowerpoint@gmail.com>
2025-04-27 01:14:55 +0700

    ok

--- lib/handler/action.ts
+++ lib/handler/action.ts
@@ -1,38 +1,48 @@
-'use server'
+
+"use server";
 
 import { ZodError, ZodSchema } from "zod";
-import { UnauthorizedError, ValidationError } from "../http-errors";
+import dbConnect from "../mongoose";
 import { auth } from "@clerk/nextjs/server";
-
+import { UnauthorizedError, ValidationError } from "../http-errors";
 type ActionOptions<T> = {
   params?: T;
   schema?: ZodSchema<T>;
   authorize?: boolean;
+  enableDb?: boolean;
+};
 
-}
+// * 1. Check if schema and params are provided and validated
+// * 2. Check if user is authorized
+// * 3. Connect to database
+// * 4. Return the params and userId.
+
 async function action<T>({
   params,
   schema,
   authorize = false,
-
+  enableDb = true,
 }: ActionOptions<T>) {
   if (schema && params) {
     try {
-      schema.parse(params)
-
+      schema.parse(params);
     } catch (error) {
       if (error instanceof ZodError) {
-        return new ValidationError(error.flatten().fieldErrors as Record<string, string[]>)
+        return new ValidationError(
+          error.flatten().fieldErrors as Record<string, string[]>
+        );
       } else {
-        return new Error("Schema validation failed")
+        return new Error("Schema validation failed");
       }
     }
   }
+  //session
   let userId: string | null = null;
   if (authorize) {
     userId = (await auth()).userId;
     if (!userId) return new UnauthorizedError();
   }
-  return { params, userId }
+  if (enableDb) await dbConnect();
+  return { params, userId };
 }
 export default action;
